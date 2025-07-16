6c93b94ebd4f2ea6ede59277e226314c16f71033
Atharv_7 <atharvwavare3563@gmail.com>
2025-07-16 17:57:43 +0530

    Add backend/src/main/java/com/example/safetymonitoring/SafetyMonitoringApplication.java 1752668862952

---
+++ backend/src/main/java/com/example/safetymonitoring/SafetyMonitoringApplication.java
@@ -0,0 +1,11 @@
+package com.example.safetymonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyMonitoringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SafetyMonitoringApplication.class, args);
+    }
+}
